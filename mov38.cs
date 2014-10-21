db4ad714d70de9f1a9cde849d562f98fa611ad2a
chema07 <chema071518@gmail.com>
2014-10-21 16:37:49 +0200

    Añadido cuadro de texto y fichero fuente para separar el codigo

---
+++ mov38.cs
@@ -0,0 +1,12 @@
+﻿
+namespace Microsoft.Samples.Kinect.SkeletonBasics
+{
+    using System.IO;
+    using System.Windows;
+    using System.Windows.Media;
+    using Microsoft.Kinect;
+
+    public partial class MainWindow : Window
+    {
+    }
+}
